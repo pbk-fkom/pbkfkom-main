6aa3207311ba0188ecdefc7923318e21dfc14e75
fikriks <fikriks130@gmail.com>
2023-08-13 21:13:39 +0700

    fix: posts sidebar :rocket:
    passing data from parent to child

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -6,11 +6,16 @@ import BlogDetailsMain from "../../components/blog-details";
 const ROOT_API = process.env.NEXT_PUBLIC_API;
 const API_VERSION = process.env.NEXT_PUBLIC_API_VERSION;
 
-const BlogDynamicDetails = ({ blog }) => {
+const BlogDynamicDetails = ({ blog, blogs, categories, tags }) => {
   return (
     <Wrapper>
       <SEO pageTitle={blog.title} />
-      <BlogDetailsMain blog={blog} />
+      <BlogDetailsMain
+        blog={blog}
+        blogs={blogs}
+        categories={categories}
+        tags={tags}
+      />
     </Wrapper>
   );
 };
@@ -19,6 +24,18 @@ export default BlogDynamicDetails;
 
 export async function getServerSideProps(context) {
   const slug = context.params.slug;
+
+  const resBlogs = await fetch(`${ROOT_API}/${API_VERSION}/posts`);
+  let blogs = await resBlogs.json();
+  blogs = blogs.data;
+
+  const resCategories = await fetch(`${ROOT_API}/${API_VERSION}/categories`);
+  let categories = await resCategories.json();
+  categories = categories.data;
+
+  const resTags = await fetch(`${ROOT_API}/${API_VERSION}/tags`);
+  let tags = await resTags.json();
+  tags = tags.data;
 
   const res = await fetch(`${ROOT_API}/${API_VERSION}/posts/${slug}`);
   let blog = await res.json();
@@ -31,5 +48,5 @@ export async function getServerSideProps(context) {
 
   blog = blog.data;
 
-  return { props: { blog } };
+  return { props: { blog, blogs, categories, tags } };
 }
